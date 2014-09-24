dbe8accf64a9cc1e591c7700ba48999623570b65
jeberst <j_eberst@yahoo.com>
2014-09-24 14:12:41 -0400

    Initial Commit of the project

---
+++ DistributedBreadthFirstSearch/GraphGenerator.cs
@@ -0,0 +1,38 @@
+﻿using Smrf.NodeXL.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Smrf.NodeXL.Layouts;
+using Smrf.NodeXL.Algorithms;
+using Smrf.NodeXL.GraphDataProviders.Facebook;
+using System.Threading;
+
+namespace DistributedBreadthFirstSearch
+{
+    public class GraphGenerator
+    {
+        public GraphGenerator() { }
+
+        public void Generator()
+        {
+            Graph oGraph = new Graph(GraphDirectedness.Undirected);
+            IVertexCollection oVertices = oGraph.Vertices;
+        }
+
+         
+        public void Importer()
+        {
+            Smrf.NodeXL.GraphDataProviders.Facebook.FacebookGraphDataProvider fbGraph = new Smrf.NodeXL.GraphDataProviders.Facebook.FacebookGraphDataProvider();
+            string data = "";
+
+            Console.WriteLine(data);
+        }
+
+        private void getFacebookData(string data, FacebookGraphDataProvider fbGraph)
+        {
+            fbGraph.TryGetGraphData(out data);
+        }
+    }
+}
---
+++ DistributedBreadthFirstSearch/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DistributedBreadthFirstSearch
+{
+    class Program
+    {
+         [STAThread]
+        static void Main(string[] args)
+        {
+            GraphGenerator graph = new GraphGenerator();
+            graph.Importer();
+        }
+    }
+}
