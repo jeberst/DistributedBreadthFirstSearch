0565badc5224ec2cac3bb753600fe2f2ad9bdcb5
JonPecoraro <JonPecoraro@gmail.com>
2014-10-05 15:48:51 -0400

    Added Breadth First Search algorithm
    This is just a basic breadth first search. It needs to be optimized to
    take in a list of parameters to be searched for, and the algorithm will
    ensure all conditions are met before returning a node.

--- DistributedBreadthFirstSearch/Program.cs
+++ DistributedBreadthFirstSearch/Program.cs
@@ -13,20 +13,61 @@ namespace DistributedBreadthFirstSearch
         static void Main(string[] args)
         {
             GraphGenerator graphGenerator = new GraphGenerator();
-            var graph = graphGenerator.Generator();
+            IGraph graph = graphGenerator.Generator();
 
             String[] metadataCollection = graph.GetRequiredValue(ReservedMetadataKeys.AllVertexMetadataKeys, typeof(String[])) as String[];
             List<string> vertexExampleValues = new List<string>();
 
             foreach (var metadata in metadataCollection)
             {
-                if (graph.Vertices.FirstOrDefault().GetValue(metadata.ToString()) != null)
+                IVertex vertex = graph.Vertices.FirstOrDefault();
+                if (vertex.GetValue(metadata) != null)
                 {
-                    vertexExampleValues.Add(graph.Vertices.FirstOrDefault().GetValue(metadata.ToString()).ToString());
+                    vertexExampleValues.Add(vertex.GetValue(metadata).ToString());
                 }
             }
 
             var ShowTheExamples = vertexExampleValues;
+
+            IVertex discoveredVertex = BreadthFirstSearch(graph, "Last Name", "Pecoraro");
+            if (discoveredVertex != null)
+            {
+                string firstName = discoveredVertex.GetValue("First Name").ToString();
+                string lastName = discoveredVertex.GetValue("Last Name").ToString();
+            }
         }
+
+         static IVertex BreadthFirstSearch(IGraph graph, string searchKey, string searchValue)
+         {
+             Queue<IVertex> queue = new Queue<IVertex>();
+             IVertex root = graph.Vertices.FirstOrDefault();
+             Dictionary<IVertex, bool> visited = new Dictionary<IVertex, bool>();
+             
+             queue.Enqueue(root);
+             visited[root] = true;
+
+             while (queue.Count > 0)
+             {
+                 IVertex currentNode = queue.Dequeue();
+
+                 string val = currentNode.GetValue(searchKey).ToString();
+                 if (currentNode.GetValue(searchKey).ToString() == searchValue)
+                 {
+                     return currentNode;
+                 }
+
+                 foreach (IEdge edge in currentNode.IncidentEdges)
+                 {
+                     IVertex child = edge.Vertex2;
+                     if (!visited.ContainsKey(child))
+                     {
+                         visited[child] = true;
+                         queue.Enqueue(child);
+                     }
+                 }
+             }
+
+             return null;
+         }
     }
 }
